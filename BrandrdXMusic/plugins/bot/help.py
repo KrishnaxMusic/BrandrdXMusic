f0e9103d885e5f7c600f38270000a88d4f50e60e
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2024-05-27 08:56:00 +0530

    King

--- BrandrdXMusic/plugins/bot/help.py
+++ BrandrdXMusic/plugins/bot/help.py
@@ -40,7 +40,7 @@ async def helper_private(
         keyboard = help_pannel(_)
         
         await update.reply_video(
-            video="https://te.legra.ph/file/e5fa97ef51ded24b993ae.mp4",
+            video="https://graph.org/file/84d30d4fd04570c0e0256.mp4",
             caption=_["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard)
 
 
